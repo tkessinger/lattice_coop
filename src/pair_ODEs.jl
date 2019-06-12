ff9c8f34fc3ab159bd6756d997621a9f01416105
Taylor Kessinger <taylor.kessinger@uky.edu>
2019-06-12 15:08:55 -0400

    improvements to numerical ODE solver

--- src/pair_ODEs.jl
+++ src/pair_ODEs.jl
@@ -9,19 +9,8 @@
 using Revise
 using DifferentialEquations
 using LatticeCoop
-using PyPlot
-
-# initiate a game
-b = 1.02
-c = 0.26
-κ = 0.1
-N = 10
-
-max_time = 10.0
-
-# initiate the population
-# technically we might only need LatticeGame for this
-pop = LatticePopulation(b, c, κ, N)
+using Plots
+pyplot()
 
 # this commented block is just to convince me that p_cd := p_dc
 
@@ -45,36 +34,47 @@ pop = LatticePopulation(b, c, κ, N)
 #     end
 # end
 
-function transaction_prob(x::Int64, y::Int64)
-    # compute the likelihood of neighbors x and y having
-    # a certain number of transactions
-    if x == 0 && y == 0
+function transaction_prob(
+    i::Int64,
+    j::Int64
+    )
+    # compute the likelihood of neighbors i and j paying
+    # a certain number of transaction costs
+    if i == 0 && j == 0
         # this can't happen: one must pay the other
         return 0.0
-    elseif x == 4 && y == 4
+    elseif i == 4 && j == 4
         # nor can this: both of them can't pay
         return 0.0
-    elseif x == 0 || y == 4
-        # this means y must have paid x
-        return binomial(3, x) * binomial(3, y-1) * 0.5^6 * 0.5
-    elseif x == 4 || y == 0
-        # this means x must have paid y
-        return binomial(3, x-1) * binomial(3, y) * 0.5^6 * 0.5
+    elseif i == 0 || j == 4
+        # this means j must have paid i
+        return binomial(3, i) * binomial(3, j-1) * 0.5^6 * 0.5
+    elseif i == 4 || j == 0
+        # this means i must have paid j
+        return binomial(3, i-1) * binomial(3, j) * 0.5^6 * 0.5
     else
         # either x or y could have paid the other
         # so consider both possibilities
-        return binomial(3, x-1) * binomial(3, y) * 0.5^6 * 0.5 +
-            binomial(3, x) * binomial(3, y-1) * 0.5^6 * 0.5
+        return binomial(3, i-1) * binomial(3, j) * 0.5^6 * 0.5 +
+            binomial(3, i) * binomial(3, j-1) * 0.5^6 * 0.5
     end
 end
 
-function H(indv_i::Bool, indv_j::Bool,
-    i_neighbors::Tuple{Bool, Bool, Bool}, j_neighbors::Tuple{Bool, Bool, Bool},
-    pop::LatticePopulation)
+function H(
+    indv_i::Bool,
+    indv_j::Bool,
+    i_neighbors::Tuple{Bool, Bool, Bool},
+    j_neighbors::Tuple{Bool, Bool, Bool},
+    game::LatticeGame
+    )
     # compute the average fermi function ("energy"/update probability)
     # between two neighbors
     # by summing over their (specified) neighbors
     # and summing over all possible transaction arrangements
+
+    # N.B.: this should return the probability that indv_j
+    # will adopt indv_i's strategy
+    # or, alternately, that indv_i will invade indv_j
 
     # this converts strategy Bool into a vector
     i_strat = [indv_i == true; indv_i == false]
@@ -87,12 +87,15 @@ function H(indv_i::Bool, indv_j::Bool,
         neighbor_strat = [k_val == true; k_val == false]
         # don't worry about the cost
         # we'll deal with that later
-        i_payoff += transpose(i_strat)*pop.game.A*neighbor_strat
+        i_payoff += transpose(i_strat)*game.A*neighbor_strat
     end
+    i_payoff += transpose(i_strat)*game.A*j_strat
     for (k, k_val) in enumerate(j_neighbors)
         neighbor_strat = [k_val == true; k_val == false]
-        j_payoff += transpose(j_strat)*pop.game.A*neighbor_strat
+        j_payoff += transpose(j_strat)*game.A*neighbor_strat
     end
+    j_payoff += transpose(j_strat)*game.A*i_strat
+
 
     average_energy = 0
 
@@ -101,24 +104,32 @@ function H(indv_i::Bool, indv_j::Bool,
     # weighted by their probability
     # this is a Binomial(3, 0.5) for each individual
     # plus a coin flip to determine who paid whom
-    for x in 0:4
-        for y in 0:4
-            prob = transaction_prob(x,y)
-            payoff_exponent = -(i_payoff - pop.game.c*x -
-                j_payoff + pop.game.c*y)/pop.game.κ
+    for i_trans in 0:4
+        for j_trans in 0:4
+            prob = transaction_prob(i_trans, j_trans)
+            payoff_exponent = ((i_payoff - game.c*i_trans) -
+                (j_payoff - game.c*j_trans))/game.κ
             average_energy += prob/(1.0+exp(payoff_exponent))
         end
     end
     return average_energy
 end
 
-function p(strat1::Bool, strat2::Bool, pair_probs::Array{Float64, 1})
+function p(
+    strat1::Bool,
+    strat2::Bool,
+    pair_probs::Array{Float64, 1}
+    )
     # this returns the p_xy pair probabilities within the sums
     return pair_probs[2*strat1+strat2+1]
 end
 
-function dot_pair_probs(d_pair_probs::Array{Float64, 1}, pair_probs::Array{Float64, 1},
-    pop::LatticePopulation, t::Float64)
+function dot_pair_probs(
+    d_pair_probs::Array{Float64, 1},
+    pair_probs::Array{Float64, 1},
+    game::LatticeGame,
+    t::Float64
+    )
     # println("$t, $pair_probs")
     # this function is a mouthful
     # it implements equations 6 and 7 from Li et al. (2019)
@@ -130,6 +141,7 @@ function dot_pair_probs(d_pair_probs::Array{Float64, 1}, pair_probs::Array{Float
     # multiplicative term preceding each sum
     # the 2 comes from symmetry
     # the ρ terms are normalizing constants
+    # note that this prefactor does not affect equilibrium dynamics
     prefactor = 2*pair_probs[2]/(ρ_c^3*ρ_d^3)
 
     # get a list of possible neighbor configurations
@@ -149,8 +161,12 @@ function dot_pair_probs(d_pair_probs::Array{Float64, 1}, pair_probs::Array{Float
         # we need p_cu, p_cv, and p_cw for this
         for (ji, j_neighbors) in enumerate(j_neighbor_configs)
             inner_sum += prod([p(true, j_neighbors[k], pair_probs) for k in 1:3]) *
-                H(true, false, j_neighbors, i_neighbors, pop)
+                H(false, true, i_neighbors, j_neighbors, game)
                 # the above should give H[P_c(u,v,w) → P_d(x,y,z)]
+                # or, in other words, the probability that
+                # j, a cooperator with neighbors (u, v, w)
+                # will adopt the strategy of
+                # i, a defector with neighbors (x, y, z)
         end
         # compute the outer sum and multiply terms by the inner sum
         # we need p_dx, p_dy, and p_dz
@@ -169,8 +185,12 @@ function dot_pair_probs(d_pair_probs::Array{Float64, 1}, pair_probs::Array{Float
         # we need p_du, p_dv, and p_dw for this
         for (ji, j_neighbors) in enumerate(j_neighbor_configs)
             inner_sum += prod([p(false, j_neighbors[k], pair_probs) for k in 1:3]) *
-                H(false, true, j_neighbors, i_neighbors, pop)
+                H(true, false, i_neighbors, j_neighbors, game)
                 # the above should give H[P_d(u,v,w) → P_c(x,y,z)]
+                # or, in other words, the probability that
+                # j, a defector with neighbors (u, v, w)
+                # will adopt the strategy of
+                # i, a cooperator with neighbors (x, y, z)
         end
         # compute the outer sum and multiply terms by the inner sum
         # we need p_cx, p_cy, and p_cz
@@ -195,11 +215,15 @@ function dot_pair_probs(d_pair_probs::Array{Float64, 1}, pair_probs::Array{Float
         # we need p_cu, p_cv, and p_cw for this
         for (ji, j_neighbors) in enumerate(j_neighbor_configs)
             inner_sum += prod([p(true, j_neighbors[k], pair_probs) for k in 1:3]) *
-                H(true, false, j_neighbors, i_neighbors, pop)
+                H(false, true, i_neighbors, j_neighbors, game)
                 # the above should give H[P_c(u,v,w) → P_d(x,y,z)]
+                # or, in other words, the probability that
+                # j, a cooperator with neighbors (u, v, w)
+                # will adopt the strategy of
+                # i, a defector with neighbors (x, y, z)
         end
         # compute the outer sum and multiply by the inner sum
-        # we need p_cu, p_cv, and p_cw for this
+        # we need p_dx, p_dy, and p_dz for this
         outer_sum += (1 - n_c) *
             prod([p(false, i_neighbors[k], pair_probs) for k in 1:3]) *
             inner_sum
@@ -210,10 +234,16 @@ function dot_pair_probs(d_pair_probs::Array{Float64, 1}, pair_probs::Array{Float
         n_c = sum(i_neighbors)
         # start computing the inner sum
         inner_sum = 0
+        # sum over all possible values of u, v, and w
+        # we need p_du, p_dv, and p_dw for this
         for (ji, j_neighbors) in enumerate(j_neighbor_configs)
             inner_sum += prod([p(false, j_neighbors[k], pair_probs) for k in 1:3]) *
-                H(false, true, j_neighbors, i_neighbors, pop)
+                H(true, false, i_neighbors, j_neighbors, game)
                 # the above should give H[P_d(u,v,w) → P_c(x,y,z)]
+                # or, in other words, the probability that
+                # j, a defector with neighbors (u, v, w)
+                # will adopt the strategy of
+                # i, a cooperator with neighbors (x, y, z)
         end
         # compute the outer sum and multiply terms by the inner sum
         # we need p_cx, p_cy, and p_cz
@@ -231,13 +261,21 @@ function dot_pair_probs(d_pair_probs::Array{Float64, 1}, pair_probs::Array{Float
     d_pair_probs[1] = -sum(d_pair_probs[2:4])
 end
 
-function solve_ODEs(b::Float64, c::Float64, κ::Float64,
-    max_time::Float64, make_plot::Bool = false)
+function solve_ODEs(
+    b::Float64,
+    c::Float64,
+    κ::Float64,
+    max_time::Float64,
+    make_plot::Bool = false
+    )
 
-    pop = LatticePopulation(b, c, κ, 10)
+    # initialize a LatticeGame
+    game = LatticeGame(b, c, κ)
+    # initial pair probabilities
     pair_probs_0 = [0.25, 0.25, 0.25, 0.25]
     tspan = (0.0, max_time)
-    prob = ODEProblem(dot_pair_probs, pair_probs_0, tspan, pop)
+    # solve the ODE
+    prob = ODEProblem(dot_pair_probs, pair_probs_0, tspan, game)
     sol = solve(prob)
     if make_plot
         fig = plt.figure()
@@ -249,35 +287,43 @@ function solve_ODEs(b::Float64, c::Float64, κ::Float64,
         ax=plt.gca()
         ax.set_xlabel("time")
         ax.set_ylabel("frequency")
-        tight_layout()
-        gcf()
+        ax.set_ylim([0.0,1.0])
+        plt.title("b = $b, c = $c, κ = $κ")
+        plt.tight_layout()
+        display(fig)
     end
-
+    # this has several attributes, including the time and the actual solution
     return sol
 end
 
-max_time = 20.0
+max_time = 500.0
+make_plots = true
 
-b_vals = collect(1.0:.02:1.06)
-c_vals = collect(0.0:.2:1.0)
+b_step = 0.02
+c_step = 0.5
+
+b_vals = collect(1.0:b_step:1.06)
+c_vals = collect(0.0:c_step:1.0)
 κ = 0.1
 ρ_c_vals = zeros(length(b_vals), length(c_vals))
 for (bi, b) in enumerate(b_vals)
     for (ci, c) in enumerate(c_vals)
         println("b = $b, c = $c")
-        sol = solve_ODEs(b, c, κ, max_time)
+        sol = solve_ODEs(b, c, κ, max_time, make_plots)
         ρ_c_vals[bi, ci] = sum(sol.u[end][3:4])
     end
 end
 
-plt.figure()
+fig = plt.figure()
+color_list = ["red", "blue", "yellow", "green", "purple", "gray", "pink"]
 for (bi, b) in enumerate(b_vals)
-    plt.plot(c_vals, ρ_c_vals[bi,:], label="b = $b")
+    plt.plot(c_vals, ρ_c_vals[bi,:], label="b = $b", c = color_list[bi])
 end
 ax=plt.gca()
 ax.set_xlabel("c")
 ax.set_ylabel(L"\rho_c")
 ax.set_ylim([0.0,0.7])
+ax.set_xlim([0.0,1.0])
 plt.legend(loc=3)
 plt.tight_layout()
-gcf()
+display(fig)
