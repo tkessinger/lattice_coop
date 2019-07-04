6e0e1ff067f4abeb2b843ef61ccae405084d05cc
Taylor Kessinger <taylor.kessinger@uky.edu>
2019-07-03 20:27:12 -0400

    birth-death and imitation updating

--- src/solve_ohtsuki_ODEs.jl
+++ src/solve_ohtsuki_ODEs.jl
@@ -27,9 +27,9 @@ function dot_x_i(
     k, w, a, update_rule = params
 
     num_strats = size(a)[1] # get the number of strategies
-    f = zeros(num_strats)
-    g = zeros(num_strats)
-    b = zeros(num_strats, num_strats)
+    f = zeros(Float64, num_strats)
+    g = zeros(Float64, num_strats)
+    b = zeros(Float64, num_strats, num_strats)
 
 
     # populate the f, g, and b arrays
@@ -42,6 +42,17 @@ function dot_x_i(
         [[b[i,j] = a[i,i] + a[i,j] -
             a[j,i] - a[j,j] for j in 1:num_strats] for i in 1:num_strats]
         b /= (k-2)
+        prefactor = w*(k-2)^2/(k-1)
+    elseif update_rule == "death_birth" || update_rule == "db"
+        [[b[i,j] = (k+1)*a[i,i] + a[i,j] -
+            a[j,i] - (k+1)*a[j,j] for j in 1:num_strats] for i in 1:num_strats]
+        b /= (k+1)*(k-2)
+        prefactor = w*(k+1)*(k-2)^2/(k*(k-1))
+    elseif update_rule == "imitation" || update_rule == "im"
+        [[b[i,j] = (k+3)*a[i,i] + 3*a[i,j] -
+            3*a[j,i] - (k+3)*a[j,j] for j in 1:num_strats] for i in 1:num_strats]
+        b /= (k+3)*(k-2)
+        prefactor = w*k*(k+3)*(k-2)^2/((k+1)^2*(k-1))
     end
 
     # g_i = \sum_k x_j b_{ij}
@@ -53,93 +64,60 @@ function dot_x_i(
     # \dot{x}_i = x_i (f_i + g_i - phi)
     # note that we need to edit the existing d_x array here rather than
     # create a new one!
-    [d_x[i] = w*(k-2)^2/(k-1) * x[i] * (f[i] + g[i] - phi) for i in 1:num_strats]
+    [d_x[i] = prefactor * x[i] * (f[i] + g[i] - phi) for i in 1:num_strats]
     println("$k, $w, $a, $f, $g, $b, $phi, $x, $d_x")
 
 end
 
-k = 5
+
+function solve_ODEs(
+    k::Union{Int64, Float64},
+    w::Union{Int64, Float64},
+    a::Array{Union{Int64, Float64}, 2},
+    update_rule::String,
+    max_time::Union{Int64, Float64},
+    make_plot::Bool = false
+    )
+
+    num_strats = size(a)[1]
+
+    # initial frequencies
+    freqs_0 = fill(1.0/num_strats, num_strats)
+    tspan = (0.0, max_time)
+    # solve the ODE
+    params = [k, w, a, "bd"]
+    prob = ODEProblem(dot_x_i, freqs_0, tspan, params)
+    sol = solve(prob)
+    if make_plot
+        fig = plt.figure()
+        labels = [L"x_c", L"x_d"]
+        for y in 1:2
+            plt.plot(sol.t, [sol.u[x][y] for x in 1:(length(sol.t))], label=labels[y])
+        end
+        legend(loc=4)
+        ax=plt.gca()
+        ax.set_xlabel("time")
+        ax.set_ylabel("frequency")
+        ax.set_ylim([0.0,1.0])
+        plt.title("k = $k")#, c = $c, κ = $κ")
+        plt.tight_layout()
+        display(fig)
+    end
+    # this has several attributes, including the time sol.t
+    # and the actual solution sol.u
+    return sol
+end
+
+
+k = 3
 w = 0.1
-a = [5 0; 8 1]
-max_time = 100.0
+R, S, T, P = 5, 0, 8, 1
+a = Float64[R S ; T P]
+update_rule = "db"
+max_time = 10000.0
 
-num_strats = size(a)[1]
-
-# initial pair probabilities
-freqs_0 = fill(1.0/num_strats, num_strats)
-#freqs_0 = [0.1, 0.9]
-tspan = (0.0, max_time)
-# solve the ODE
-params = [k, w, a, "bd"]
-prob = ODEProblem(dot_x_i, freqs_0, tspan, params)
-sol = solve(prob)
-
-
-# function solve_ODEs(
-#     k::Float64,
-#     w::Float64,
-#     a::Array{Float64, 2},
-#     update_rule::String,
-#     max_time::Float64,
-#     make_plot::Bool = false
-#     )
-#
-#     num_strats = size(a)[1]
-#
-#     # initial pair probabilities
-#     freqs_0 = fill(1.0/num_strats, num_strats)
-#     tspan = (0.0, max_time)
-#     # solve the ODE
-#     prob = ODEProblem(dot_x_i, freqs_0, tspan, k, w, a)
-#     sol = solve(prob)
-#     if make_plot
-#         fig = plt.figure()
-#         labels = [L"p_{dd}", L"p_{dc}", L"p_{cd}", L"p_{cc}"]
-#         for y in reverse(1:4)
-#             plt.plot(sol.t, [sol.u[x][y] for x in 1:(length(sol.t))], label=labels[y])
-#         end
-#         legend(loc=4)
-#         ax=plt.gca()
-#         ax.set_xlabel("time")
-#         ax.set_ylabel("frequency")
-#         ax.set_ylim([0.0,1.0])
-#         plt.title("b = $b, c = $c, κ = $κ")
-#         plt.tight_layout()
-#         display(fig)
-#     end
-#     # this has several attributes, including the time sol.t
-#     # and the actual solution sol.u
-#     return sol
-# end
-#
-# max_time = 20.0
-# make_plots = true
-#
-# b_step = 0.01
-# c_step = 0.1
-#
-# b_vals = collect(1.0:b_step:1.06)
-# c_vals = collect(0.0:c_step:1.0)
-# κ = 0.1
-# ρ_c_vals = zeros(length(b_vals), length(c_vals))
-# for (bi, b) in enumerate(b_vals)
-#     for (ci, c) in enumerate(c_vals)
-#         println("b = $b, c = $c")
-#         sol = solve_ODEs(b, c, κ, max_time, make_plots)
-#         ρ_c_vals[bi, ci] = sum(sol.u[end][3:4])
-#     end
-# end
-#
-# fig = plt.figure()
-# color_list = ["red", "blue", "yellow", "green", "purple", "gray", "pink"]
-# for (bi, b) in enumerate(b_vals)
-#     plt.plot(c_vals, ρ_c_vals[bi,:], label="b = $b", c = color_list[bi])
-# end
-# ax=plt.gca()
-# ax.set_xlabel("c")
-# ax.set_ylabel(L"\rho_c")
-# ax.set_ylim([0.0,0.7])
-# ax.set_xlim([0.0,1.0])
-# plt.legend(loc=3)
-# plt.tight_layout()
-# display(fig)
+#w_range = Float64.(10 .^(range(-3,stop=0,length=10)))
+#for (wi, w) in enumerate(w_range)
+    #print(w)
+    solve_ODEs(k, w, a, update_rule, max_time, true)
+#end
