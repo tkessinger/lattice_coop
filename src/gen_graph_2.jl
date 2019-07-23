e0687255be09dc2c66fbe49ae2a7ea98813653d3
Taylor Kessinger <tkess@sas.upenn.edu>
2019-07-23 17:20:37 -0400

    multitype graph generation works

--- src/gen_graph_2.jl
+++ src/gen_graph_2.jl
@@ -11,11 +11,8 @@
 
 using Random, StatsBase, Combinatorics, Revise
 
-N = 8
-k = 6
-g1 = 2
-g2 = 2
-g3 = 2
+N = 500
+g = [1, 1]
 
 
 function is_suitable(
@@ -131,9 +128,13 @@ function generate_multitype_random_regular(
     N::Int64, # number of individuals
     g::Array{Int64, 1} # list of number of edges of each type
     )
+    # generates a graph (list of neighbors for each individual)
+    # in which there are multiple edge types
 
-    # list of all edges (undirected)
-    all_edges = Array{Int64,1}[]
+    # sum(g) needs to be less than N
+    if sum(g) > N
+        throw(DomainError(g, "sum(g) must be <= N"))
+    end
 
     # list of edges by type
     # edges[i][j] should give the jth edge of type i
@@ -155,10 +156,14 @@ function generate_multitype_random_regular(
             push!(neighbors[i][s2], s1)
         end
     end
-    # sort the neighbor list to make it easier to read
-    [[sort!(neighbs) for neighbs in neighbors[i]] for i in 1:length(g)]
-    return neighbors
 
+    all_neighbors = [Int64[] for i in 1:N]
+    [[[push!(all_neighbors[indv], neighbor) for neighbor in neighbors[ii][indv]] for indv in 1:N] for ii in 1:length(g)]
+
+    # sort the neighbor list to make it easier to read
+    [sort!(neighbs) for neighbs in all_neighbors]
+    [[sort!(neighbs) for neighbs in neighbors[i]] for i in 1:length(g)]
+    return neighbors, all_neighbors
 end
 
 function recursive_graph_gen(
@@ -166,6 +171,12 @@ function recursive_graph_gen(
     g::Array{Int64, 1}, # number of edges of each type
     ii::Int64 # current type of edge we are trying to generate
     )
+    # attempt to generate a multitype-edge regular graph
+    # if this turns out to be impossible
+    # (e.g., because one type of edge is assigned in a way that
+    # makes other types impossible)
+    # then return an empty list for that type
+
     edges = Array{Array{Int64,1},1}[]
     # recursively call this function if we haven't ``hit'' the lowest level yet
     if ii > 1
@@ -180,7 +191,71 @@ function recursive_graph_gen(
     return edges
 end
 
-g = [g1, g2]
-gg = generate_multitype_random_regular(N, [g1, g2, g3]);
+function assemble_connected_list(
+    all_neighbors::Array{Array{Int64,1},1},
+    curr_connections::Array{Int64,1},
+    indv::Int64,
+    )
+    if indv ∉ curr_connections
+        push!(curr_connections, indv)
+    end
+    for neighb in all_neighbors[indv]
+        if neighb ∉ curr_connections
+            push!(curr_connections, neighb)
+            #println("$indv, $neighb, $(vcat(tmp_connections, curr_connections ...))")
+            new_connections = assemble_connected_list(all_neighbors, curr_connections, neighb)
+            #println("$new_connections")
+            #new_connections = assemble_connected_list(all_neighbors, curr_connections, neighb)
+            [push!(curr_connections, x) for x in new_connections if x ∉ curr_connections]
+        end
+    end
+    return vcat(curr_connections ...)
+end
+
+function is_connected(
+    neighbors::Array{Array{Array{Int64,1},1},1}
+    )
+    all_neighbors = [Int64[] for i in 1:N]
+    [[[push!(all_neighbors[indv], neighbor) for neighbor in neighbors[ii][indv]] for indv in 1:N] for ii in 1:length(neighbors)]
+    return is_connected(all_neighbors)
+end
+
+function is_connected(
+    all_neighbors::Array{Array{Int64,1},1},
+    )
+    N = length(all_neighbors)
+    connected_list = assemble_connected_list(all_neighbors, Int64[], 1)
+    connected_list = sort(unique(connected_list))
+    #println("$connected_list")
+    if length(connected_list) == N
+    #if connected_list == collect(1:N)
+        return true
+    else
+        return false
+    end
+end
+
+function generate_multitype_connected_graph(
+    N::Int64, # number of individuals
+    g::Array{Int64, 1} # list of number of edges of each type
+    )
+    num_attempts = 1
+    let
+    connected = false
+    while !(connected)
+        global gg = generate_multitype_random_regular(N, g)
+        connected = is_connected(gg[2])
+        num_attempts += 1
+        println("$num_attempts")#, $(gg[2])")
+    end
+    end
+    return gg
+end
+
+gg = generate_multitype_connected_graph(N, g)
+#gg = generate_multitype_random_regular(N, g)
+#is_connected(gg[2])
+#assemble_connected_list([[2,3],[1,3],[1,2]], Int64[], 1)
+#assemble_connected_list(gg[2], Int64[], 1)
 
 #println("$edges")
